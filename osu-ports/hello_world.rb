76124823ad9b6900fc7b98bc279989efbe637003
James Spolsdoff <jspolsdoff@gmail.com>
2015-06-15 14:33:10 +0000

    changed dir structure

---
+++ osu-ports/hello_world.rb
@@ -0,0 +1,3 @@
+# This is just a basic "Hello World!" program
+
+puts "Hello World!"
---
+++ osu-ports/simple_io.rb
@@ -0,0 +1,8 @@
+# This is a program that asks the user for their name and display a welcome message
+
+puts "Hello, what is your name? "
+
+name = gets
+name = name.chomp
+
+puts "Glad you ran my Ruby program " + name + "."
