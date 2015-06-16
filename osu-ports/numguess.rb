c702b37c95d7021ae39945ff82ebeb6fcb5ce5b1
James Spolsdoff <jspolsdoff@gmail.com>
2015-06-16 02:10:51 +0000

    input validation is working

---
+++ osu-ports/numguess.rb
@@ -0,0 +1,38 @@
+# Author:           		    James Spolsdoff
+# Dated Created:                    6/15/15
+# Last Modified Date:               6/15/15
+# Filename:                         numGuess.rb
+#
+# Overview:
+# This is a two player number guessing game. One player will be asked for a 
+# secret number. The next player will have three guesses to get the number
+# right. The program will keep track of the guess and let the player know if they
+# too high, low, or exactly correct.
+
+require 'io/console'
+
+puts "\nWELCOME TO THE NUMBER GUESSING GAME\n\n"
+		
+puts "This is a two player number guessing game. The first player will select the secret number."
+puts "The second player will have three chances to guess the number. After every guess you will be told
+if your guess is too high, too low, or spot on.\n"
+
+current_player = 1
+
+begin
+  
+  print "\nPlayer " + current_player.to_s + " please enter a secret number from 1 - 100: "
+  secret_number = STDIN.noecho(&:gets).to_i  
+
+  while secret_number > 100  || secret_number < 1 do
+    print "Your input is out of range. Please enter a number between 1 - 100: "
+    secret_number = STDIN.noecho(&:gets).to_i
+    puts "\n"
+  end
+
+  puts "\n\nThanks! Now let's get this game starts."
+
+  print "Do you want to play again(Y/N): "
+  play_again = gets.chomp.upcase
+
+end until play_again == "N" 
